c7cf87661fe1b6ddf66caeee58da7162134e4b15
rum <rumeshranaweera99@gmail.com>
2026-05-20 19:12:24 +0530

    move environment variable to .env

--- frontend/src/lib/firebase/firebase-config.ts
+++ frontend/src/lib/firebase/firebase-config.ts
@@ -1,11 +1,11 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyC4zwpo7df2qXTj558D25UJoSfWWDiMZN4",
-  authDomain: "lanka-low-bot.firebaseapp.com",
-  projectId: "lanka-low-bot",
-  storageBucket: "lanka-low-bot.firebasestorage.app",
-  messagingSenderId: "801656599569",
-  appId: "1:801656599569:web:480d65f77e3edcae4c2db0",
-  measurementId: "G-TDBTW9G5T8",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 export const AUTH_COOKIE_NAME = "auth-token";
